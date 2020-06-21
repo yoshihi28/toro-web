9a2c9db92b3ae9dd5591e6c6e992a439b34bfd6a
yoshihi <trkrlvds28@gmail.com>
2020-06-22 01:25:09 +0900

    test

--- js/common.js
+++ js/common.js
@@ -31,7 +31,7 @@ function changeImage() {
 		count = 0;
 	}
 	document.pixelart.src = "img/" + img[count];
-	setTimeout("changeImage()", 500);
+	setTimeout("changeImage()", 100);
 }
 
 changeImage();
